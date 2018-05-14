96f369481bca9ffffadc2ba6614cf2d2450b6c81
Andy Leeb <andrew@onna.com>
2018-05-14 09:42:05 -0400

    remove unused

--- main.go
+++ main.go
@@ -20,12 +20,7 @@ type Address struct {
     State string `json:"state,omitempty"`
 }
 
-// populate a people slice
 var people []Person
-//people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-//people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-//people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
 
 func GetPeople(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(people)
